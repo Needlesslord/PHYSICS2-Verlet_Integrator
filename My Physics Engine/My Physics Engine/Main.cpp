035b574ecb4d0fcf71cf73a0ab569efee2ca64af
EnricGDV <minenime4thewin@gmail.com>
2019-11-15 12:01:13 +0100

    time fixed

--- My Physics Engine/My Physics Engine/Main.cpp
+++ My Physics Engine/My Physics Engine/Main.cpp
@@ -6,19 +6,17 @@ int main()
 	//VerletIntegrator::Object object1;
 	//object1.vx = 5;
 	//object1.x = 5;
-	float xi = 5;
-	float vi = 5;
-	float v = 0;
+	float v = 5;
 	float a = 1;
-	float x = 0;
+	float x = 5;
 
-	for (int t = 0; t < 60; t++)
+	for (int t = 1, i = 0; i < 60; i++)
 	{
 		//VerletIntegrator::Integrate(object1, 1);
-		x = xi + v*t + (a / 2)*t*t;
-		v = vi + a*t;
+		x = x + v*t + (a / 2)*t*t;
+		v = v + a*t;
 
-		std::cout << "x: " << x << "  v: " << v << "  a: " << a << " t: "<< t << std::endl;
+		std::cout << "x: " << x << "  v: " << v << "  a: " << a << " t: "<< i << std::endl;
 	}
 
 	system("pause");
