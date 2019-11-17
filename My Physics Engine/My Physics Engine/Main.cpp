d180cd0e6bc25f4c09f99d0470e62d261e88f7ee
EnricGDV <minenime4thewin@gmail.com>
2019-11-17 21:41:04 +0100

    merge

--- My Physics Engine/My Physics Engine/Main.cpp
+++ My Physics Engine/My Physics Engine/Main.cpp
@@ -6,17 +6,25 @@ int main()
 	//VerletIntegrator::Object object1;
 	//object1.vx = 5;
 	//object1.x = 5;
-	float v = 5;
-	float a = 1;
-	float x = 5;
+	//float xi = 0.0;
+	//float vi = 5.0;
+	float v = 1.0;
+	float a = 1.0;
+	float x = 0.0;
+	float fr = 30.0;
+	float dt = 0.0;
 
-	for (int t = 1, i = 0; i < 60; i++)
+	for (int numF = 0; numF < 60; numF++)
 	{
-		//VerletIntegrator::Integrate(object1, 1);
-		x = x + v*t + (a / 2)*t*t;
-		v = v + a*t;
+		//calculate the dt: time between frames
+		dt = 1.0f / fr;							//1 --> int, 1.0 --> float
 
-		std::cout << "x: " << x << "  v: " << v << "  a: " << a << " t: "<< i << std::endl;
+		//VerletIntegrator::Integrate(object1, 1);
+
+
+		x = x + v * dt + (a * 0.5f)*(dt*dt);		// 0.5 better than 1/2
+		v = v + a * dt;
+		std::cout << "x: " << x << "  v: " << v << "  a: " << a << "  frame: " << numF << std::endl;
 	}
 
 	system("pause");
